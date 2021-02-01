9bbc337b2d32f5787bf43340ce689d61d6c99c5e
Tal Oved <toptal2@walla.com>
2021-02-01 21:55:56 +0200

    testing

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -14,7 +14,6 @@ export class EditExpensePage extends React.Component{
         this.props.history.push('/');
     }
     render(){
-        console.log(this.props);
         return (
             <div>
                 <ExpenseForm 
