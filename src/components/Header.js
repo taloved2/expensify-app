0973af8ff4ad9e4af404c66663723444c49ddecc
Tal Oved <toptal2@walla.com>
2021-02-01 20:06:00 +0200

    setup build and server and deleting help page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true} >Home</NavLink>
         <NavLink to="/create" activeClassName="is-active" >Create Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active" >Help</NavLink>
     </header>
 );
 export default Header;
