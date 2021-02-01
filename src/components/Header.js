f3c16b5bbb0461cb0ac4af47e7586ab9c4645deb
Tal Oved <toptal2@walla.com>
2021-02-01 19:39:16 +0200

    Setup production build and server

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true} >Home</NavLink>
-        <NavLink to="/create" activeClassName="is-active" >Add Expense</NavLink>
+        <NavLink to="/create" activeClassName="is-active" >Create Expense</NavLink>
         <NavLink to="/help" activeClassName="is-active" >Help</NavLink>
     </header>
 );
