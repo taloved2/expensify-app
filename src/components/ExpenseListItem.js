996f17267cf45865958f00edf1881f2c363fc0c1
Tal Oved <toptal2@walla.com>
2021-02-11 20:32:30 +0200

    Finished Expensify App

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
-const Item = ({id, description, amount, createdAt}) =>(
+const Item = ({id, description, amount, createdAt,note}) =>(
         <Link className="list-item" to={`/edit/${id}`} >
             <div>
                 <h3 className="list-item__title" >description: {description}</h3>
                 <span className="list-item__subtitle">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+                {note&&<p className="list-item__subdata">notes: {note}</p>}
             </div>
             <h3 className="list-item__data">
                 {numeral(amount/100).format('$0,0.00')}
